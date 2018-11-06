465edd874022f648486aef44c26848a9e9a7dc97
Dr Nic Williams <drnicwilliams@gmail.com>
2018-11-06 16:03:06 +1000

    show any files inside /secrets folder

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ class App < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + '/public'
 
   get "/" do
-    erb :index
+    secrets_base_dir = ENV["SECRETS_BASE_DIR"]
+    secrets_base_dir ||= "/tmp/secrets"
+    erb :index, locals: {secrets_base_dir: secrets_base_dir}
   end
 end
